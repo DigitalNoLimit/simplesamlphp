b633f2a246d09358343a85e505016d43ace1cee4
oGirodinDnl <olivier.girodin@digitalnolimit.com>
2018-01-24 15:03:03 +0100

    Adding composer.json

--- lib/SimpleSAML/Metadata/MetaDataStorageHandlerFlatFile.php
+++ lib/SimpleSAML/Metadata/MetaDataStorageHandlerFlatFile.php
@@ -146,6 +146,13 @@ class SimpleSAML_Metadata_MetaDataStorageHandlerFlatFile extends SimpleSAML_Meta
 		}
 	}
 
+    /**
+     * @param string $set
+     */
+    public function addValidSet(string $set)
+    {
+        self::$validSets[] = $set;
+    }
 
 }
 
